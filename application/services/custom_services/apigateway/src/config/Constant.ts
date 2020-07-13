fa2a53fb31293821ad4e4ee4405fdc6606a1f1a2
naveenlingam <naveenlingam@10decoders.in>
2020-07-13 13:19:28 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://tabslee-app.tabslee.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://tabslee-app.tabslee.svc.cluster.local:3009';
+export const ADMINURL = 'http://tabslee-app.tabslee.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://tabslee-app.tabslee.svc.cluster.local:3008';
